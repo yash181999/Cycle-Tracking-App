dad8517649c39bfdbc22c0e9afa41e89c9f95398
Yash Mehta <yash.m@boardingware.com>
2024-03-28 17:50:41 +0530

    First Commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,150 @@
+"use client";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { useEffect, useRef, useState } from "react";
+import Map, { GeolocateControl, Layer, useMap } from "react-map-gl";
+import mapboxgl, { LngLatBoundsLike } from "mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+import Source from "react-map-gl/dist/esm/components/source";
+import { getDistance } from "geolib";
+
+type Location = {
+  latitude: number | undefined;
+  longitude: number | undefined;
+};
+
+// Replace 'YOUR_MAPBOX_ACCESS_TOKEN' with your actual MapBox access token
+
+interface Coord {
+  lng: number;
+  lat: number;
+}
 
 export default function Home() {
+  const mapContainer = useRef<HTMLDivElement>(null);
+  const [tracking, setTracking] = useState<boolean>(false);
+  const [path, setPath] = useState<Coord[]>([]);
+  const [timeElapsed, setTimeElapsed] = useState<number>(0);
+  const [showMap, setShowMap] = useState<boolean>(false);
+
+  const startTracking = () => {
+    setPath([]);
+    setTracking(true);
+    navigator.geolocation.watchPosition(
+      (position) => {
+        const newCoords: Coord = {
+          lng: position.coords.longitude,
+          lat: position.coords.latitude,
+        };
+        setPath((prevPath) => [...prevPath, newCoords]);
+      },
+      (error) => console.error(error),
+      { enableHighAccuracy: true }
+    );
+  };
+
+  const stopTracking = () => {
+    setTracking(false);
+    setShowMap(true);
+  };
+
+  const pauseTracking = () => {
+    setTracking(false);
+  };
+
+  const getDistanceTravelled = () => {
+    let totalDistance = 0;
+    for (let i = 0; i < path.length - 1; i++) {
+      totalDistance += getDistance(
+        { latitude: path[i].lat, longitude: path[i].lng },
+        { latitude: path[i + 1].lat, longitude: path[i + 1].lng }
+      );
+    }
+    return totalDistance * 0.001;
+  };
+
+  // start a timer to calculate the time elapsed
+  useEffect(() => {
+    let interval = undefined;
+    if (tracking) {
+      interval = setInterval(() => {
+        setTimeElapsed((prevTime) => prevTime + 1);
+      }, 1000);
+    } else {
+      clearInterval(interval);
+    }
+
+    return () => clearInterval(interval);
+  }, [tracking]);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div className="flex justify-center items-center flex-col p-20">
+      <div className="border-4 border-solid border-sky-500">
+        {showMap && path.length > 0 && (
+          <Map
+            mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}
+            initialViewState={{
+              longitude: path[0].lng,
+              latitude: path[0].lat,
+              zoom: 50,
+            }}
+            style={{ width: "500px", height: "500px" }}
+            mapStyle="mapbox://styles/mapbox/streets-v11"
+            // disable the default attribution
+            attributionControl={false}
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+            <Source
+              id="polylineLayer"
+              type="geojson"
+              data={{
+                type: "Feature",
+                properties: {},
+                geometry: {
+                  type: "LineString",
+                  coordinates: path.map((coord) => [coord.lng, coord.lat]),
+                },
+              }}
+            >
+              <Layer
+                id="lineLayer"
+                type="line"
+                source="polylineLayer"
+                layout={{
+                  "line-join": "round",
+                  "line-cap": "round",
+                }}
+                paint={{
+                  "line-color": "red",
+                  "line-width": 8,
+                }}
+              ></Layer>
+            </Source>
+            <GeolocateControl showAccuracyCircle showUserLocation />
+          </Map>
+        )}
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="flex items-center gap-4">
+        <Button className="bg-[#67BF00]" onClick={startTracking}>
+          Start a new Ride
+        </Button>
+
+        <Button className="bg-[#67BF00]" onClick={startTracking}>
+          Pause Ride
+        </Button>
+
+        <Button className="bg-red-500" onClick={stopTracking}>
+          Stop Ride
+        </Button>
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div>Time Elapsed: {timeElapsed * 0.0166667}</div>
+      <div>
+        Current Speed: {(getDistanceTravelled() / timeElapsed) * 0.0166667}
       </div>
-    </main>
+
+      <button onClick={stopTracking}>Stop Tracking</button>
+      <div>Total Distance {getDistanceTravelled()}</div>
+    </div>
   );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  disable: false,
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+});
+
+const nextConfig = withPWA({});
 
 export default nextConfig;
